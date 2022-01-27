5132e31852519aa5ce201d3294cb3fc458c06e2a
Prapti Pandit <prapti.pandit@sophos.com>
2022-01-27 22:14:23 +0530

    Fix indentation

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -11,22 +11,22 @@ void swap (int *a, int *b) {
 }
 
 int partition(int *array, int l, int  h) {
-	int pivot = array[l];
-	int i = l;
-	int j = h;
+    int pivot = array[l];
+    int i = l;
+    int j = h;
 
     while  (i < j) {
-		do{
-			i++;
+        do{
+            i++;
         } while (array[i] <= pivot);
-		cout << "i:" << i <<", ";
+        cout << "i:" << i <<", ";
         do {
-		    j--;
-		} while (array[j] > pivot);
-		cout << "j:" << j << ", ";
-		if (i < j) {
-		    swap(&array[i], &array[j]);
-		}
+            j--;
+        } while (array[j] > pivot);
+        cout << "j:" << j << ", ";
+        if (i < j) {
+            swap(&array[i], &array[j]);
+        }
     }
     //swap old pivot with new  pivot
     swap(&array[l], &array[j]);
@@ -37,26 +37,28 @@ int partition(int *array, int l, int  h) {
 void quick_sort(int *array, int l, int h) {
     int pivot;
     cout << "quick sort call for l,h :" << l << " ,"<< h << endl;
-	if (l < h) {
-		pivot = partition(array, l, h);
-		cout << "pivot: " << pivot << endl;
-		quick_sort(array, l, pivot);
-		quick_sort(array, pivot+1, h);
+    if (l < h) {
+        pivot = partition(array, l, h);
+        cout << "pivot: " << pivot << endl;
+        quick_sort(array, l, pivot);
+        quick_sort(array, pivot+1, h);
     }
 }
 
 int main() {
-	int size = 9;
-	int array[] = {10,16,8,12,15,6,3,9,5,INT_MAX};
-	for (int i = 0; i < size; i++) {
-		cout << array[i] << ", ";
+    int size = 9;
+    int array[] = {10,16,8,12,15,6,3,9,5,INT_MAX};
+
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << ", ";
     }
     cout << endl;
+
     quick_sort(array,0,size);
+
     for (int i = 0; i < size; i++) {
-		cout << array[i] << ", ";
+        cout << array[i] << ", ";
     }
-	return 0;
+
+    return 0;
 }
-
-
